3c1c33424721dbfb59a00a5f1e099dff0c6e1550
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-03-27 19:46:26 -0400

    included email validation

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // alert("helllloooo");
+
+//Declare Variables
+
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const form = document.querySelector("form");
+
+//Declare Functions
+
+// function handleError (){
+
+// }
+
+//Call Functions
+
+// form.addEventListener("submit", function (e) {
+// 	e.preventDefault();
+// 	localStorage.setItem;
+// });
+
+username.addEventListener("blur", function (e) {
+	if (username.value === "") {
+		username.setCustomValidity("ENTER A USERNAME");
+	} else {
+		username.setCustomValidity("");
+	}
+	username.reportValidity();
+});
+email.addEventListener("blur", function (e) {
+	if (email.value === "") {
+		email.setCustomValidity("ENTER A USERNAME");
+	} else {
+		email.setCustomValidity("");
+	}
+	email.reportValidity();
+});
